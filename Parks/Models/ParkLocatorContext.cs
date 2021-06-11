d8b820bab62577f5a5dc14a3a78ad27d5b73f59d
thomasfriedrichs <friedrichs.tommy@gmail.com>
2021-06-11 12:15:30 -0700

    edit seeds

--- Parks/Models/ParkLocatorContext.cs
+++ Parks/Models/ParkLocatorContext.cs
@@ -13,11 +13,11 @@ namespace ParkLocator.Models
         {
           builder.Entity<Park>()
               .HasData(
-                  new Park { ParkId = 1, Name = "Acadia", State = "Maine", YearFounded = 1919, YearlyVisitors = "3437286" },
+                  new Park { ParkId = 1, Name = "Acadia", State = "Maine", YearFounded = 1919, YearlyVisitors = "3500000" },
                   new Park { ParkId = 2, Name = "American Samoa", State = "American Samoa", YearFounded = 1988, YearlyVisitors = "60000" },
-                  new Park { ParkId = 3, Name = "Arches", State = "Utah", YearFounded = 1971, YearlyVisitors = "1659702" },
-                  new Park { ParkId = 4, Name = "Pip", State = "Shark", YearFounded = 4, YearlyVisitors = "Male" },
-                  new Park { ParkId = 5, Name = "Bartholomew", State = "Dinosaur", YearFounded = 22, YearlyVisitors = "Male" }
+                  new Park { ParkId = 3, Name = "Arches", State = "Utah", YearFounded = 1971, YearlyVisitors = "1600000" },
+                  new Park { ParkId = 4, Name = "Badlands", State = "South Dakota", YearFounded = 1978, YearlyVisitors = "970000" },
+                  new Park { ParkId = 5, Name = "Big Bend", State = "Texas", YearFounded = 1944, YearlyVisitors = "460000" }
               );
         }
 
